739794d42bb1ac7b24eee444bb7a7e4ea6c8016c
Emily Liew <53126513+emilyy-liew@users.noreply.github.com>
2022-10-30 13:26:02 -0400

    Add files via upload
    Can solve a medium sudoku puzzle on NYT! Added algorithms include eliminating numbers in rows, columns, and 3 By 3's if a certain combination of numbers can only be filled in a certain number of boxes that matches the number of numbers in the combination.

--- Puzzle.java
+++ Puzzle.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Puzzle {
-    static int[][][][] puzzle;
-    static int[][] countOfEach;
-    private static boolean solved;
+    int[][][][] puzzle;
+    int[][] countOfEach;
+    private boolean solved;
 
     Puzzle() {
         puzzle = new int[3][3][3][3];
@@ -17,7 +20,7 @@ public class Puzzle {
         return solved;
     }
 
-    public static void input(int rowNumbers, int row) {
+    public void input(int rowNumbers, int row) {
         int number = 100000000;
         for (int x = 0; x < 3; x++) {
             for (int z = 0; z < 3; z++) {
@@ -28,7 +31,7 @@ public class Puzzle {
         }
     }
 
-    public static void output() {
+    public void output() {
         System.out.printf("%n-------------------------------------%n");
         for (int w = 0; w < 3; w++) {
             for (int y = 0; y < 3; y++) {
@@ -42,7 +45,7 @@ public class Puzzle {
         }
     }
 
-    public static void fillDraft() {
+    public void fillDraft() {
         for (int w = 0; w < 3; w++) {
             for (int y = 0; y < 3; y++) {
                 for (int x = 0; x < 3; x++) {
@@ -55,12 +58,14 @@ public class Puzzle {
         }
     }
 
-    public static void scanPuzzle() {
+    public void scanPuzzle() {
         for (int w = 0; w < 3; w++) {
             for (int y = 0; y < 3; y++) {
                 for (int x = 0; x < 3; x++) {
                     for (int z = 0; z < 3; z++) {
-                        solved = checkSolved();
+                        if (checkSolved() || !checkCanSolve()) {
+                            break;
+                        }
                         if(puzzle[w][x][y][z] < 10) {
                             eliminateNumberinRow(puzzle[w][x][y][z], w, y);
                             eliminateNumberinColumn(puzzle[w][x][y][z], x, z);
@@ -76,18 +81,22 @@ public class Puzzle {
                     }
                 }
                 setOnlyOneinRow(w, y);
+                sortRow(w, y);
             }
         }
+        sortColumnAnd3By3();
         setOnlyOneinCol();
     }
 
-    public static void eliminateNumberInBox(int eliminate, int w, int x, int y, int z) {
+    public void eliminateNumberInBox(int eliminate, int w, int x, int y, int z) {
         if (Integer.toString(puzzle[w][x][y][z]).indexOf(Integer.toString(eliminate)) != -1) {
-            puzzle[w][x][y][z] = Integer.valueOf(Integer.toString(puzzle[w][x][y][z]).substring(0, Integer.toString(puzzle[w][x][y][z]).indexOf(Integer.toString(eliminate))) + Integer.toString(puzzle[w][x][y][z]).substring(Integer.toString(puzzle[w][x][y][z]).indexOf(Integer.toString(eliminate)) + 1));
+            String first = Integer.toString(puzzle[w][x][y][z]).substring(0, Integer.toString(puzzle[w][x][y][z]).indexOf(Integer.toString(eliminate)));
+            String last = Integer.toString(puzzle[w][x][y][z]).substring(Integer.toString(puzzle[w][x][y][z]).indexOf(Integer.toString(eliminate)) + 1);
+            puzzle[w][x][y][z] = Integer.valueOf(first + last);
         }
     }
 
-    public static void eliminateNumberinRow(int eliminate, int w, int y) {
+    public void eliminateNumberinRow(int eliminate, int w, int y) {
         for (int x = 0; x < 3; x++) {
             for (int z = 0; z < 3; z++) {
                 if (puzzle[w][x][y][z] > 9) {
@@ -97,7 +106,7 @@ public class Puzzle {
         }
     }
 
-    public static void eliminateNumberinColumn(int eliminate, int x, int z) {
+    public void eliminateNumberinColumn(int eliminate, int x, int z) {
         for (int w = 0; w < 3; w++) {
             for (int y = 0; y < 3; y++) {
                 if (puzzle[w][x][y][z] > 9) {
@@ -107,7 +116,7 @@ public class Puzzle {
         }
     }
 
-    public static void eliminateNumberin3By3(int eliminate, int w, int x) {
+    public void eliminateNumberin3By3(int eliminate, int w, int x) {
         for (int y = 0; y < 3; y++) {
             for (int z = 0; z < 3; z++) {
                 if(puzzle[w][x][y][z] > 9) {
@@ -117,7 +126,50 @@ public class Puzzle {
         }
     }
 
-    public static void setOnlyOneinRow(int w, int y) {
+    private void eliminateRowWithoutSafety(HashSet<Integer> safe, int eliminate, int w, int y) {
+        for (int x = 0; x < 3; x++) {
+            for (int z = 0; z < 3; z++) {
+                if (safe.contains(z + x * 3) || puzzle[w][x][y][z] < 10) {
+                    // do nothing
+                } else {
+                    for (int l = 0; l < Integer.toString(eliminate).length(); l++) {
+                        eliminateNumberInBox(Integer.valueOf(Integer.toString(eliminate).substring(l, l+1)), w, x, y, z);
+                    }
+                }
+            }
+        }
+    }
+
+    private void eliminateColumnWithoutSafety(HashSet<Integer> safe, int eliminate, int x, int z) {
+        for (int w = 0; w < 3; w++) {
+            for (int y = 0; y < 3; y++) {
+                if (safe.contains(y + w * 3) || puzzle[w][x][y][z] < 10) {
+                    // do nothing
+                } else {
+                    for (int l = 0; l < Integer.toString(eliminate).length(); l++) {
+                        eliminateNumberInBox(Integer.valueOf(Integer.toString(eliminate).substring(l, l+1)), w, x, y, z);
+                    }
+                }
+            }
+        }
+    }
+
+    private void eliminateR3By3WithoutSafety(HashSet<Integer> safe, int eliminate, int w, int x) {
+        for (int y = 0; y < 3; y++) {
+            for (int z = 0; z < 3; z++) {
+                if (safe.contains(z + y * 3) || puzzle[w][x][y][z] < 10) {
+                    // do nothing
+                } else {
+                    for (int l = 0; l < Integer.toString(eliminate).length(); l++) {
+                        eliminateNumberInBox(Integer.valueOf(Integer.toString(eliminate).substring(l, l+1)), w, x, y, z);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void setOnlyOneinRow(int w, int y) {
         for (int c = 0; c < 9; c++) {
             if (countOfEach[0][c] == 1) {
                 for (int x = 0; x < 3; x++) {
@@ -132,7 +184,7 @@ public class Puzzle {
         clearCountRow();
     }
 
-    public static void setOnlyOneinCol() {
+    public void setOnlyOneinCol() {
         for (int r = 1; r < 10; r++) {
             for (int c = 0; c < 9; c++) {
                 if (countOfEach[r][c] == 1) {
@@ -148,8 +200,9 @@ public class Puzzle {
         }
         clearCount();
     }
-
-    public static boolean checkSolved() {
+    // ONLY ONES IN 3 BY 3
+    // TODO: COMBINE CHECKSOLVED WITH CANCHECKSOLVE
+    public boolean checkSolved() {
         for (int w = 0; w < 3; w++) {
             for (int y = 0; y < 3; y++) {
                 for (int x = 0; x < 3; x++) {
@@ -164,18 +217,211 @@ public class Puzzle {
         return true;
     }
 
-    public static void clearCountRow() {
+    // TODO: IMPLEMENT THIS
+    public boolean checkCanSolve() {
+        for (int w = 0; w < 3; w++) {
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    for (int z = 0; z < 3; z++) {
+                        if(puzzle[w][x][y][z] == 0) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public void clearCountRow() {
         for(int c = 0; c < 9; c++) {
             countOfEach[0][c] = 0;
         }
     }
 
-    public static void clearCount() {
+    public void clearCount() {
         for(int r = 0; r < 9; r++) {
             for(int c = 0; c < 9; c++) {
                 countOfEach[r][c] = 0;
             }
         }
     }
-    // sort
+
+    public void sortRow(int w, int y) {
+        int[] sorted = new int[9];
+        int[] indices = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        int singles = 0;
+
+        for (int x = 0; x < sorted.length; x++) {
+            sorted[x] = puzzle[w][x/3][y][x%3];
+        }
+
+        mergeSort(sorted, indices, 0, sorted.length - 1);
+
+        for (int x = 0; x < sorted.length - 1; x++) {
+            HashSet<Integer> count = new HashSet<Integer>();
+            HashSet<Integer> safety = new HashSet<Integer>();
+            if (sorted[x] < 10) {
+                singles++;
+            } else {
+                if (Integer.toString(sorted[x]).length() != 9 - singles) {
+                    for (int a = 0; a < Integer.toString(sorted[x]).length(); a++) {
+                        count.add(Integer.valueOf(Integer.toString(sorted[x]).substring(a, a+1)));
+                    }
+                    safety.add(indices[x]);
+                    for (int z = x + 1; z < sorted.length; z++) {
+                        for (int a = 0; a < Integer.toString(sorted[z]).length(); a++) {
+                            count.add(Integer.valueOf(Integer.toString(sorted[z]).substring(a, a+1)));
+                        }
+                        safety.add(indices[z]);
+                        if (count.size() == safety.size() && count.size() != 9 - singles) {
+                            for (int i: count) {
+                                eliminateRowWithoutSafety(safety, i, w, y);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void sortColumnAnd3By3() {
+        int[] sorted = new int[9];
+
+        for (int x = 0; x < 3; x++) {
+            for (int z = 0; z < 3; z++) {
+                for (int w = 0; w < 3; w++) {
+                    for (int y = 0; y < 3; y++) {
+                        sorted[y + w * 3] = puzzle[w][x][y][z];
+                    }
+                    sort3By3(w, x);
+                }
+                int singles = 0;
+                int[] indices = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+                mergeSort(sorted, indices, 0, sorted.length - 1);
+
+                for (int a = 0; a < sorted.length - 1; a++) {
+                    HashSet<Integer> count = new HashSet<Integer>();
+                    HashSet<Integer> safety = new HashSet<Integer>();
+                    if (sorted[a] < 10) {
+                        singles++;
+                    } else {
+                        if (Integer.toString(sorted[a]).length() != 9 - singles) {
+                            for (int b = 0; b < Integer.toString(sorted[a]).length(); b++) {
+                                count.add(Integer.valueOf(Integer.toString(sorted[a]).substring(b, b + 1)));
+                            }
+                            safety.add(indices[a]);
+                            for (int c = a + 1; c < sorted.length; c++) {
+                                for (int b = 0; b < Integer.toString(sorted[c]).length(); b++) {
+                                    count.add(Integer.valueOf(Integer.toString(sorted[c]).substring(b, b + 1)));
+                                }
+                                safety.add(indices[c]);
+                                if (count.size() == safety.size() && count.size() != 9 - singles) {
+                                    for (int i: count) {
+                                        eliminateColumnWithoutSafety(safety, i, x, z);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void sort3By3(int w, int x) {
+        int[] sorted = new int[9];
+        int[] indices = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        int singles = 0;
+
+        for (int y = 0; y < sorted.length; y++) {
+            sorted[y] = puzzle[w][x][y/3][y%3];
+        }
+
+        mergeSort(sorted, indices, 0, sorted.length - 1);
+
+        for (int a = 0; a < sorted.length - 1; a++) {
+            HashSet<Integer> count = new HashSet<Integer>();
+            HashSet<Integer> safety = new HashSet<Integer>();
+            if (sorted[a] < 10) {
+                singles++;
+            } else {
+                if (Integer.toString(sorted[a]).length() != 9 - singles) {
+                    for (int b = 0; b < Integer.toString(sorted[a]).length(); b++) {
+                        count.add(Integer.valueOf(Integer.toString(sorted[a]).substring(b, b + 1)));
+                    }
+                    safety.add(indices[a]);
+                    for (int z = a + 1; z < sorted.length; z++) {
+                        for (int b = 0; b < Integer.toString(sorted[z]).length(); b++) {
+                            count.add(Integer.valueOf(Integer.toString(sorted[z]).substring(b, b + 1)));
+                        }
+                        safety.add(indices[z]);
+                        if (count.size() == safety.size() && count.size() != 9 - singles) {
+                            for (int i: count) {
+                                eliminateR3By3WithoutSafety(safety, i, w, x);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // private void eliminateOnlyInRowIn3By3()
+    // private void eliminateOnlyInColoumnIn3By3()
+
+    public void mergeSort(int[] toSort, int[] indices, int left, int right) {
+        if (left < right) {
+            int mid = (right - left)/2 + left;
+
+            mergeSort(toSort, indices, left, mid);
+            mergeSort(toSort, indices, mid + 1, right);
+
+            merge(toSort, indices, left, mid, right);
+        }
+    }
+
+    public void merge(int[] toSort, int[] indices, int left, int mid, int right) {
+        int[] leftArray = new int[mid - left + 1];
+        int[] leftIndex = new int[mid - left + 1];
+        int[] rightArray = new int[right - mid];
+        int[] rightIndex = new int[right - mid];
+
+        for (int x = 0; x < leftArray.length; x++) {
+            leftArray[x] = toSort[left + x];
+            leftIndex[x] = indices[left + x];
+        }
+
+        for (int x = 0; x < rightArray.length; x++) {
+            rightArray[x] = toSort[mid + 1 + x];
+            rightIndex[x] = indices[mid + 1 + x];
+        }
+
+        int i = 0, j = 0;
+        while (i < leftArray.length && j < rightArray.length) {
+            if (leftArray[i] <= rightArray[j]) {
+                toSort[left + i + j] = leftArray[i];
+                indices[left + i + j] = leftIndex[i];
+                i++;
+            } else {
+                toSort[left + i + j] = rightArray[j];
+                indices[left + i + j] = rightIndex[j];
+                j++;
+            }
+        }
+
+        while (i < leftArray.length) {
+            toSort[left + i + j] = leftArray[i];
+            indices[left + i + j] = leftIndex[i];
+            i++;
+        }
+
+        while (j < rightArray.length) {
+            toSort[left + i + j] = rightArray[j];
+            indices[left + i + j] = rightIndex[j];
+            j++;
+        }
+
+    }
 }
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -4,18 +4,23 @@ public class SudokuSolver {
     public static void main(String[] args) {
         Puzzle puzzle = new Puzzle();
         Scanner in = new Scanner(System.in);
+        // int[] easy = {19300082, 800149, 85104000, 1037820, 932000010, 500060403, 800700231, 190000700, 700618000};
+        // int[] medium = {800312040, 200040000, 6000500, 590000, 20000051, 60000000, 400000030, 1780000, 80000906};
+        // int[] hard = {960000800, 50000, 1400000, 200, 250006000, 39105000, 8502, 310000400, 6000013};
 
         for (int x = 0; x < 9; x++) {
             System.out.print("Please input the numbers of row " + (x+1) + " with 0's for the blank spaces (Ex: 700000100): ");
             puzzle.input(in.nextInt(), x);
             in.nextLine();
+            //puzzle.input(medium[x], x);
         }
         puzzle.fillDraft();
+        int x = 0;
         do {
             puzzle.scanPuzzle();
-        } while (!(puzzle.getSolved()));
+            x++;
+        } while (!(puzzle.getSolved()) && x < 30);
         puzzle.output();
-
     }
 }
 // same number of unique elements as boxes they take up
